0acf195f8c9e4ab70f7f379c1766c2abcaad5978
Natali Angelova <natali.v.angelova@gmail.com>
2022-08-25 18:58:43 +0300

    Update ArrarAdvanced lab

--- ArraysAdvanced/lab.js
+++ ArraysAdvanced/lab.js
@@ -8,11 +8,91 @@ function solve01(arr) {
 
 //02. Negative or Positive Numbers
 function solve02(arr) {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    const element = arr[i];
+  let newArray = [];
+  for (const item of arr) {
+    let element = Number(item);
     if (element < 0) {
+      newArray.unshift(element);
+    } else {
+      newArray.push(element);
     }
+  }
+  console.log(newArray.join("\n"));
+}
+
+//03. First and Last K Numbers
+function solve03(arr) {
+  let kNum = arr.shift();
+  let firstElement = [];
+  let lastElement = [];
+  for (let f = 0; f < kNum; f++) {
+    firstElement.push(arr[f]);
+    lastElement.unshift(arr[arr.length - 1 - f]);
+  }
+
+  console.log(firstElement.join(" "));
+  console.log(lastElement.join(" "));
+}
+
+//04. Last K Numbers Sequance
+function solve04(n, k) {
+  let arr = [];
+  arr.push(1);
+  while (n > 1) {
+    let count = k;
+    let sum = 0;
+    for (let index = arr.length - 1; index >= 0; index--) {
+      if (count > 0) {
+        sum += arr[index];
+        count--;
+      }
+    }
+    arr.push(sum);
+    n--;
+  }
+  console.log(arr.join(" "));
+}
+
+//5.	Process Odd Numbers
+function solve05(arr) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index++) {
+    if (index % 2 !== 0) {
+      newArr.push(arr[index] * 2);
+    }
+  }
+  newArr.reverse();
+  console.log(newArr.join(" "));
+}
+
+function solve05new(numbers) {
+  (numbers) =>
+    numbers
+      .filter((el, i) => i % 2 === 1)
+      .map((x) => x * 2)
+      .reverse()
+      .join(" ");
+}
+
+//06. Smallest Two Numbers
+function solve06(arr) {
+  let sortingElement = arr.sort((a, b) => {
+    return a - b;
+  });
+  let smallestElement = sortingElement.slice(0, 2);
+
+  console.log(smallestElement.join(" "));
+}
+
+//07. List Of Products
+function solve07(arr) {
+  let sortArr = arr.sort((a, b) => a.localeCompare(b));
+  for (let i = 0; i < sortArr.length; i++) {
+    console.log(`${i + 1}.${sortArr[i]}`);
   }
 }
 
-solve02(["7", "-2", "8", "9"]);
+//08. Array Manipulations
+function solve08(arr) {
+
+solve07(["Potatoes", "Tomatoes", "Onions", "Apples"]);
